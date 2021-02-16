65ccff42ccffec44856ed77b70380f26c9765113
Nazz095 <63593136+Nazz095@users.noreply.github.com>
2021-02-16 12:15:13 +0100

    Motore + joystick

--- Progetti_SCA/Miei/02-16_DC_Motor_Joystick/02-16_DC_Motor_Joystick.ino
+++ Progetti_SCA/Miei/02-16_DC_Motor_Joystick/02-16_DC_Motor_Joystick.ino
@@ -3,10 +3,11 @@ Exercise the motor using
 the L293D chip and joystick
 ***************************/
 
-//considero soltanto l'asse X del joystick collegato ad A0
-const byte X_pin = 0;
-//imposto i pin del motore
-const byte ENABLE = 5, DIRA = 3, DIRB = 4;
+//considero soltanto l'asse X del joystick collegato ad A0 e la tolleranza in mezzo
+const byte X_pin = 0, TOLLERANZA = 5;
+//imposto i pin del motore e la sua velocita'
+const byte ENABLE = 5, DIRA = 3, DIRB = 4, SPEED_MIN = 65, SPEED_MAX = 255;
+const unsigned short VAL_MEDIO = 508, VAL_MAX = 1023;
 
 byte i, fanSpeed, dir;
 unsigned short x_value;
@@ -26,17 +27,17 @@ void loop() {
   x_value = analogRead(X_pin);
 
   //muovo joystick verso i propri pin (per me verso destra)
-  if(x_value > 510){
+  if(x_value > VAL_MEDIO + TOLLERANZA){
     digitalWrite(DIRA,HIGH);
     digitalWrite(DIRB,LOW);
-    fanSpeed = map (x_value, 510, 1023, 65, 255);
+    fanSpeed = map (x_value, VAL_MEDIO + TOLLERANZA, VAL_MAX, SPEED_MIN, SPEED_MAX);
     dir = 1;
   }
   //muovo joystick lontano dai propri pin (per me verso sinistra)
-  else if (x_value < 505){
+  else if (x_value < VAL_MEDIO - TOLLERANZA){
     digitalWrite(DIRA,LOW);
     digitalWrite(DIRB,HIGH);
-    fanSpeed = map (x_value, 505, 0, 65, 255);
+    fanSpeed = map (x_value, VAL_MEDIO - TOLLERANZA, 0, SPEED_MIN, SPEED_MAX);
     dir = 2;
   }
   //joystick sta fermo in centro, nella sua posizione neutrale
